2e93fac08a20ae56f78bdd530729423614c36ac2
Luiz Fillipe <78454639+LuizFillipe1@users.noreply.github.com>
2024-02-20 14:39:09 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,77 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-        autoplay: {
-            delay: 3000, // Tempo em milissegundos entre as transições
-            disableOnInteraction: false // Permite que a reprodução automática continue mesmo se o usuário interagir com o carrossel
-        },
-        loop: true,
-        effect: 'fade', // Adiciona um efeito de fade
-        fadeEffect: {
-            crossFade: true // Ativa a transição de fade
-        }
-    });
+// Script home
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var swiper = new Swiper('.swiper-container', {
+//         slidesPerView: 3,
+//         spaceBetween: 10,
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev'
+//         },
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true
+//         },
+//         autoplay: {
+//             delay: 3000, // Tempo em milissegundos entre as transições
+//             disableOnInteraction: false // Permite que a reprodução automática continue mesmo se o usuário interagir com o carrossel
+//         },
+//         loop: true,
+//         effect: 'fade', // Adiciona um efeito de fade
+//         fadeEffect: {
+//             crossFade: true // Ativa a transição de fade
+//         }
+//     });
+// });
+var swiper = new Swiper('.swiper-home-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2000,
+        // pauseOnMouseEnter: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+    },
+    loop: true,
 });
 
-// Obtém o tamanho da tela
-var larguraTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var alturaTela = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-// Exibe o tamanho da tela no console
-console.log('Largura da tela: ' + larguraTela + ' pixels');
-console.log('Altura da tela: ' + alturaTela + ' pixels');
 
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-depoimentos-container', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-        autoplay: {
-            delay: 3000, // Tempo em milissegundos entre as transições
-            disableOnInteraction: false // Permite que a reprodução automática continue mesmo se o usuário interagir com o carrossel
-        },
-        // loop: true,
 
-        effect: 'fade', // Adiciona um efeito de fade
-        fadeEffect: {
-            crossFade: true // Ativa a transição de fade
-        }
-    });
+
+// Script depoimentos
+
+var swiper = new Swiper('.swiper-depoimentos-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 20,
+        pauseOnMouseEnter: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+    },
 });
---
+++ js/teste.js
@@ -0,0 +1,11 @@
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var swiper = new Swiper('.swiper-container', {
+//         slidesPerView: 3, // Número de slides visíveis
+//         spaceBetween: 20, // Espaço entre os slides
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//         },
+//     });
+// });
