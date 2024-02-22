c8997c9343b1efc2727ad852ebecb3f6aea8ed3f
Luiz Fillipe <78454639+LuizFillipe1@users.noreply.github.com>
2024-02-22 12:41:06 -0300

    update

---
+++ js/projetos.js
@@ -0,0 +1,24 @@
+// var swiper = new Swiper('.home-swiper-container', {
+//     slidesPerView: 3,
+//     spaceBetween: 10,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 2000,
+//         // pauseOnMouseEnter: true,
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     effect: 'coverflow',
+//     coverflowEffect: {
+//         rotate: 0,
+//         slideShadows: false,
+//     },
+//     loop: true,
+// });
+
--- js/scripts.js
+++ js/scripts.js
@@ -72,3 +72,22 @@ var swiper = new Swiper('.swiper-depoimentos-container', {
         slideShadows: false,
     },
 });
+
+
+// Projetos
+
+const galleryItems = document.querySelectorAll('.gallery-items');
+
+// Adiciona um ouvinte de eventos a cada elemento da galeria
+galleryItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        // Obtém o elemento filho com a classe 'hover-text' dentro do item da galeria
+        const hoverText = item.querySelector('.hover-text');
+        hoverText.style.display = 'block';
+    });
+
+    item.addEventListener('mouseout', () => {
+        const hoverText = item.querySelector('.hover-text');
+        hoverText.style.display = 'none';
+    });
+});
