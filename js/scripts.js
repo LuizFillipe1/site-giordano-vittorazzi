9853c0cc95b9aa31b968f97acf7de657d8574e22
Luiz Fillipe <78454639+LuizFillipe1@users.noreply.github.com>
2024-03-01 17:27:32 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-// Script home
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     var swiper = new Swiper('.swiper-container', {
-//         slidesPerView: 3,
-//         spaceBetween: 10,
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev'
-//         },
-//         pagination: {
-//             el: '.swiper-pagination',
-//             clickable: true
-//         },
-//         autoplay: {
-//             delay: 3000, // Tempo em milissegundos entre as transições
-//             disableOnInteraction: false // Permite que a reprodução automática continue mesmo se o usuário interagir com o carrossel
-//         },
-//         loop: true,
-//         effect: 'fade', // Adiciona um efeito de fade
-//         fadeEffect: {
-//             crossFade: true // Ativa a transição de fade
-//         }
-//     });
-// });
 var swiper = new Swiper('.home-swiper-container', {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -49,11 +24,6 @@ var swiper = new Swiper('.home-swiper-container', {
     loop: true,
 });
 
-// const myCarouselElement = document.querySelector('#carousel')
-
-// const carousel = new bootstrap.Carousel(myCarouselElement, {
-//     interval: 7,
-// })
 
 // Script depoimentos
 
@@ -62,8 +32,6 @@ var swiper = new Swiper('.swiper-depoimentos-container', {
     spaceBetween: 10,
     initialSlide: 2,
     // centeredSlides: true,
-
-
     // autoplay: {
     //     delay: 2000,
     //     pauseOnMouseEnter: true,
