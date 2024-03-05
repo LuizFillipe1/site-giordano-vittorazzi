31d7e937ccaf8b1f95d03a0ade230d8290aac24b
Luiz Fillipe <78454639+LuizFillipe1@users.noreply.github.com>
2024-03-05 00:47:10 -0300

    update index with responsive img

--- js/home.js
+++ js/home.js
@@ -2,10 +2,11 @@ var swiper = new Swiper('.home-swiper-container', {
     slidesPerView: 1,
     spaceBetween: 10,
 
-    autoplay: {
-        delay: 2500,
-        // pauseOnMouseEnter: true,
-    },
+
+    // autoplay: {
+    //     delay: 2500,
+    //     // pauseOnMouseEnter: true,
+    // },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
